b0f248e93531f70e87efd938cc721347f64fc902
Sujan TR <sujan.tr@carvewing.com>
2025-11-01 16:10:37 +0530

    First Project simple landing page

---
+++ 01_brand_page/src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+function Navigation() {
+  return (
+    <>
+      <nav className="container">
+        <div className="logo">
+          <img src="../public/images/brand_logo.png" alt="brand_logo" />
+        </div>
+
+        <ul>
+          <li href="#">MENU</li>
+          <li href="#">LOCATION</li>
+          <li href="#">ABOUT</li>
+          <li href="#">CONTACT</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </>
+  );
+}
+
+export default Navigation;
